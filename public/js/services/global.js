a4ac83161567a294f5468e3bbafb9fb4dbb1e270
David Nimmo <dnimmo@gmail.com>
2013-09-08 22:30:10 +0100

    Models for frontend updated

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,4 +1,15 @@
 window.angular.module('ngff.services.global', [])
   .factory('Global', function() {
-
+  	//Login/logout is a synchronous server action, so this service is refreshed every time the authentication state changes - Injecting the 'Global' service into the application gives the app access to the current user
+  	var current_user = window.user;
+  	//Return an object with methods that indirectly interact with the current_user object
+  	return {
+  		current_user: function(){
+  			return current_user;
+  		},
+  		isSignedIn: function(){
+  			//!! converts current_user to boolean - so global.isSignedIn() will return true if "current_user" exists (i.e. if the user is signed in)
+  			return !!current_user;
+  		}
+  	};
   });
